3cf66397a08c4088fd80c4383d7697e50e14add5
frobenius7 <vetb@me.com>
2021-11-22 21:06:09 +0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const CONTRACT_NAME = process.env.CONTRACT_NAME || 'guest-book.testnet';
+const CONTRACT_NAME = process.env.CONTRACT_NAME || 'vetal.testnet';
 
 function getConfig(env) {
   switch(env) {
